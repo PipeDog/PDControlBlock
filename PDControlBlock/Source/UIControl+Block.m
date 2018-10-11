75bc5a2e2af080e3e39d7525edb0fac053bf5cd4
leiliang <1007279249@qq.com>
2018-10-11 15:07:41 +0800

    commit

--- PDControlBlock/Source/UIControl+Block.m
+++ PDControlBlock/Source/UIControl+Block.m
@@ -9,63 +9,69 @@
 #import "UIControl+Block.h"
 #import <objc/runtime.h>
 
+@interface PDActionWrapper : NSObject
+
+@property (nonatomic, copy) void (^block)(__kindof UIControl *);
+@property (nonatomic, assign) UIControlEvents controlEvents;
+
+- (void)triggerAction:(id)sender;
+
+@end
+
+@implementation PDActionWrapper
+
+- (void)triggerAction:(id)sender {
+    !self.block ?: self.block(sender);
+}
+
+@end
+
 @interface UIControl ()
 
-@property (nonatomic, strong) NSMutableDictionary<NSNumber *, NSMutableArray<void (^)(__kindof UIControl *)> *> *allActions;
+@property (nonatomic, strong) NSMutableDictionary<NSNumber *, NSMutableArray<PDActionWrapper *> *> *allWrappers;
 
 @end
 
 @implementation UIControl (Block)
 
-static void eventHandlerImpl(id self, SEL op, id target) {
-    NSString *selName = NSStringFromSelector(op);
-    NSUInteger toIndex = [selName rangeOfString:@":"].location;
-    NSUInteger fromIndex = @"eventHandler_".length;
-    
-    NSString *controlEventsKey = [selName substringWithRange:NSMakeRange(fromIndex, toIndex - fromIndex)];
-    
-    UIControl *control = (UIControl *)self;
-    NSMutableArray<void (^)(__kindof UIControl *)> *actionsForControlEvents = control.allActions[@([controlEventsKey integerValue])];
-    
-    for (void (^block)(UIControl *) in actionsForControlEvents) {
-        if (block) block(control);
-    }
-}
-
 - (void)addActionForControlEvents:(UIControlEvents)controlEvents usingBlock:(void (^)(__kindof UIControl * _Nonnull))block {
-    if (!block) {
-        return;
-    }
-
-    NSMutableArray<void (^)(__kindof UIControl *)> *actionsForControlEvents = self.allActions[@(controlEvents)];
     
-    if (!actionsForControlEvents) {
-        actionsForControlEvents = [NSMutableArray array];
-        
-        SEL sel = NSSelectorFromString([NSString stringWithFormat:@"eventHandler_%lu:", controlEvents]);
-        class_addMethod([self class], sel, (IMP)eventHandlerImpl, "v@:@");
-        [self addTarget:self action:sel forControlEvents:controlEvents];
+    NSAssert(block, @"Param block can not be nil!");
+    
+    NSMutableArray<PDActionWrapper *> *wrappersForControlEvents = self.allWrappers[@(controlEvents)];
+    if (!wrappersForControlEvents) {
+        wrappersForControlEvents = [NSMutableArray array];
     }
     
-    [actionsForControlEvents addObject:[block copy]];
-    self.allActions[@(controlEvents)] = actionsForControlEvents;
+    PDActionWrapper *wrapper = [[PDActionWrapper alloc] init];
+    wrapper.block = block;
+    wrapper.controlEvents = controlEvents;
+    [self addTarget:wrapper action:@selector(triggerAction:) forControlEvents:controlEvents];
+    
+    [wrappersForControlEvents addObject:wrapper];
+    self.allWrappers[@(controlEvents)] = wrappersForControlEvents;
 }
 
 - (void)removeActionsForControlEvents:(UIControlEvents)controlEvents {
-    NSMutableArray<void (^)(__kindof UIControl *)> *actionsForControlEvents = self.allActions[@(controlEvents)];
-    if (!actionsForControlEvents.count) return;
     
-    [actionsForControlEvents removeAllObjects];
+    NSMutableArray<PDActionWrapper *> *wrappersForControlEvents = self.allWrappers[@(controlEvents)];
+    if (!wrappersForControlEvents.count) { return; }
+
+    for (PDActionWrapper *wrapper in wrappersForControlEvents) {
+        [self removeTarget:wrapper action:@selector(triggerAction:) forControlEvents:wrapper.controlEvents];
+    }
+    
+    [wrappersForControlEvents removeAllObjects];
 }
 
 #pragma mark - Getter Methods
-- (NSMutableDictionary<NSString *, NSMutableArray<void (^)(UIControl *)> *> *)allActions {
-    NSMutableDictionary *allActions = objc_getAssociatedObject(self, _cmd);
-    if (!allActions) {
-        allActions = [NSMutableDictionary dictionary];
-        objc_setAssociatedObject(self, _cmd, allActions, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+- (NSMutableDictionary<NSNumber *,NSMutableArray<PDActionWrapper *> *> *)allWrappers {
+    NSMutableDictionary *_allWrappers = objc_getAssociatedObject(self, _cmd);
+    if (!_allWrappers) {
+        _allWrappers = [NSMutableDictionary dictionary];
+        objc_setAssociatedObject(self, _cmd, _allWrappers, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     }
-    return allActions;
+    return _allWrappers;
 }
 
 @end
